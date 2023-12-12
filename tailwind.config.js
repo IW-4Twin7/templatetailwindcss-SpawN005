b13e1739ec75a02d17876fcdfc25c15135589be6
SpawN 005 <jasserbouzidi22@gmail.com>
2023-12-12 16:30:28 +0100

    finsihed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,22 @@ module.exports = {
   content: ["./src/*"],
   theme: {
     extend: {
-     
-
-
-      colors:{
-
-        navColor:"#314584",
-        btnColor:"#ff3946",
-        pColor:"#6e757a"
+      colors: {
+        navColor: "#314584",
+        btnColor: "#ff3946",
+        pColor: "#6e757a",
+        cardColor: "#ff3946",
       },
-      fontSize:{
-
-        navSize:"24px",
-        titleSize:"74px",
-        pSize:"18px"
+      fontSize: {
+        navSize: "24px",
+        titleSize: "74px",
+        pSize: "18px",
       },
 
-      fontFamily:{
-
-        fontNav:["Work Sans"]
-      }
+      fontFamily: {
+        fontNav: ["Work Sans"],
+      },
     },
   },
   plugins: [],
-}
-
+};
